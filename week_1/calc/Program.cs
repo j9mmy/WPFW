ebd7fef7abc02fa7b8b6b6f218ab5ecc529d6cad
j9mmy <127404933+j9mmy@users.noreply.github.com>
2024-10-16 18:43:53 +0200

    add (week 1): calc

---
+++ week_1/calc/Program.cs
@@ -0,0 +1,68 @@
+﻿class SomInt
+{
+    private int _waarde;
+    public int waarde { 
+        get
+        {
+            return _waarde;
+        }
+        private set
+        {
+            if (value > 2147483647)
+            {
+                throw new ArgumentOutOfRangeException("waarde", "Waarde is te groot");
+            }
+
+            _waarde = value;
+        }
+    }
+
+    public SomInt(int waarde)
+    {        
+        this.waarde = waarde * 4;
+    }
+
+    public SomInt(int waarde1, int waarde2)
+    {
+        this.waarde = waarde1 + waarde2;
+    }
+
+    // Calc 2
+    public static SomInt operator +(SomInt som1, SomInt som2)
+    {
+        return new SomInt(som1.waarde + som2.waarde);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SomInt som;
+        int input1 = 0;
+        int input2 = 0;
+        string input;
+
+        bool success = false;
+
+        while(!success)
+        {
+            Console.WriteLine("Voer het eerste getal in: ");
+            input = Console.ReadLine();
+            success = int.TryParse(input, out input1);
+
+            if (!success) Console.WriteLine("Geen (geldige) getal ingevoerd.");
+        }
+
+        Console.WriteLine("Voer een tweede getal (indien van toepassing): ");
+        input = Console.ReadLine();
+        success = int.TryParse(input, out input2);
+
+        som = success ? new SomInt(input1) : new SomInt(input1, input2);
+
+        Console.WriteLine("Resultaat: " + som.waarde);
+
+        // SomInt som3 = som1 + som2;
+        // Console.WriteLine(som3.waarde);
+    }
+}
