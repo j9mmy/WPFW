0ea3a2254034f9b007f71c0f587a4070c2ea199d
j9mmy <127404933+j9mmy@users.noreply.github.com>
2024-10-16 18:44:51 +0200

    add (week 2): telwoorden

---
+++ week_2/telwoorden/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string filePath = @"./txt/input.txt";
+        List<string> text = new List<string>();
+
+        try{
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line = "";
+                int lineNumber = 0;
+
+                while((line = reader.ReadLine()) != null)
+                {
+                    text.Add(line);
+                }
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine("The file was not found: " + ex.Message);
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine("You do not have permission to access this file: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+
+        Console.WriteLine(string.Join("\n", text));
+    }
+}
